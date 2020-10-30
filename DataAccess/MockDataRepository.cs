68b62d36374c87559169032f5dc8b8d9ce2272bd
Brandon Moore <brandonvmoore@gmail.com>
2020-10-30 12:44:14 -0500

    Removed hard coded data

--- DataAccess/MockDataRepository.cs
+++ DataAccess/MockDataRepository.cs
@@ -16,42 +16,6 @@ namespace SkillsAssessment.DataAccess
         }
 
         static List<CategoryModel> CategoryList = new List<CategoryModel>();
-        //{
-        //    new CategoryModel() { 
-        //        Id = 1, 
-        //        Title = "Leadership", 
-        //        Quote = "If you delegate tasks you create followers.\n\nIf you delegate authority you create leaders.",
-        //        Competencies = new List<CompetencyModel>() 
-        //        {
-        //            //new CompetencyModel() { Id = 1, Title = "Team Size", Question = "Note the team sizes you’ve led and want to lead" },
-        //            //new CompetencyModel() { Id = 2, Title = "Company Size", Question = "Note the company sizes you’ve worked at & want to work for" },
-        //            new CompetencyModel() 
-        //            { 
-        //                Id = 3, 
-        //                Title = "Impact", 
-        //                Question = "How do you impact organizations?" ,
-        //                Skills = new List<SkillModel>()
-        //                {
-        //                    new SkillModel() { Id = 1, Text = "Build things from scratch" },
-        //                    new SkillModel() { Id = 2, Text = "Manage growing companies" },
-        //                    new SkillModel() { Id = 3, Text = "Business Process Optimization" },
-        //                    new SkillModel() { Id = 4, Text = "Governance, Risk & Compliance" },
-        //                    new SkillModel() { Id = 5, Text = "Improve underperforming orgs" },
-        //                    new SkillModel() { Id = 6, Text = "Get projects back on track" },
-        //                    new SkillModel() { Id = 7, Text = "Maintain smooth running teams" },
-        //                    new SkillModel() { Id = 8, Text = "Right-sizing/Down-sizing" },
-        //                    new SkillModel() { Id = 9, Text = "M&A - selling" },
-        //                    new SkillModel() { Id = 10, Text = "M&A - buying" },
-        //                    new SkillModel() { Id = 11, Text = "Prep for IPO" }
-        //                }
-        //            },
-        //            new CompetencyModel() { Id = 4, Title = "Talented At Leading", Question = "Who are you skilled at leading?"}
-        //        } 
-        //    },
-        //    new CategoryModel() { Id = 2, Title = "Communication" },
-        //    new CategoryModel() { Id = 3, Title = "Productivity" },
-        //};
-
         public IEnumerable<CategoryModel> GetCategoryModels()
         {
             return CategoryList;
